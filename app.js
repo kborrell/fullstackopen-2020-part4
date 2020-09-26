13904123e1710ad4dc6502dc478c5a1da4adfbbc
Kevin Borrell <kborrells@gmail.com>
2020-09-26 13:06:44 +0200

    Completed exercises 4.1 and 4.2

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+mongoose.connect(config.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+})
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
